8c1ab0f94f8314f1a3a767b624f4c5afb5abf7cd
Mohamed Saeed <muhameds913@gmail.com>
2025-07-09 01:31:55 +0300

    fix

--- Areas/Identity/Controllers/AccountController.cs
+++ Areas/Identity/Controllers/AccountController.cs
@@ -4,6 +4,9 @@ using Mapster;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+using System.Threading.Tasks;
 
 
 namespace ETickets.Areas.Identity.Controllers
@@ -14,11 +17,13 @@ namespace ETickets.Areas.Identity.Controllers
 
         private readonly UserManager<ApplicationUser> _UserManager;
         private readonly IEmailSender _EmailSender;
-
-        public AccountController(UserManager<ApplicationUser> userManager, IEmailSender emailSender)
+        private readonly SignInManager<ApplicationUser> _SignInManager;
+        public AccountController(UserManager<ApplicationUser> userManager, IEmailSender emailSender, SignInManager<ApplicationUser> signInManager)
         {
             _UserManager = userManager;
             _EmailSender = emailSender;
+            _SignInManager = signInManager;
+
         }
 
 
@@ -39,14 +44,8 @@ namespace ETickets.Areas.Identity.Controllers
             if (result.Succeeded)
             {
                 //Send confirmation email.
-                var token = await _UserManager.GenerateEmailConfirmationTokenAsync(user);
-
-                var link = Url.Action("ConfrimEmail", "Account", new { userId = user.Id,  token, area = "Identity" }, Request.Scheme);  // Request.Scheme the protocal
-
-                await _EmailSender.SendEmailAsync(user.Email ?? "", "Confirm your account", $"<h1> Confirm your account by clicking <a href='{link}'> here </ a> </ h1>");
-
-                TempData["Success"] = "Please confirm your email";
-
+                await SendToken(user);
+                TempData["Success"] = "Confirm your email";
             } else
             {
                 foreach (var error in result.Errors)
@@ -56,14 +55,86 @@ namespace ETickets.Areas.Identity.Controllers
                 return View(registerVM);
             }
 
-            return RedirectToAction("Index", "Home", new { area = "Customer" });
+            return RedirectToAction(nameof(Login));
+        }
+        [HttpGet]
+        public async Task SendToken(ApplicationUser user)
+        {
+
+            var token = await _UserManager.GenerateEmailConfirmationTokenAsync(user);
+
+            var link = Url.Action("ConfrimEmail", "Account", new { userId = user.Id, token, area = "Identity" }, Request.Scheme);  // Request.Scheme the protocal
+
+            await _EmailSender.SendEmailAsync(user.Email ?? "", "Confirm your account", $"<h1> Confirm your account by clicking <a href='{link}'> here </ a> </ h1>");
+
+         
+        }
+
+        public async Task<JsonResult> SendEmailConfirmationByUserNameOrEmail(string UserNameOrEmail)
+        {
+            var user = await _UserManager.FindByEmailAsync(UserNameOrEmail);
+
+            user ??= await _UserManager.FindByNameAsync(UserNameOrEmail);
+
+            if(user is not null)
+            {
+               await SendToken(user);
+                TempData["Success"] = "Confirm your email";
+                return new JsonResult(new { status = true });
+            }
+            TempData["Success"] = "Somthing went wrong!";
+            return new JsonResult(new { status = false });
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new LoginVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        
+        
+        {
+            var user = await _UserManager.FindByEmailAsync(loginVM.UserNameOrEmail);
+
+            user ??= await _UserManager.FindByNameAsync (loginVM.UserNameOrEmail);
+
+            if (user is not null)
+            {
+                var result = await _UserManager.CheckPasswordAsync(user, loginVM.Password);
+
+                if (result)
+                {
+                    if(!user.EmailConfirmed)
+                    {
+                        TempData["error"] = "Confirm your email.";
+                        loginVM.EmailConfirmed = false;
+                        TempData["UserNameOrEmail"] = loginVM.UserNameOrEmail;
+                        return View(loginVM);
+                    }
+                    if (!user.LockoutEnabled)
+                    {
+                        TempData["error"] = $"Account blocked. {user.LockoutEnd}.";
+                        return View(loginVM);
+                    }
+
+                    TempData["success"] = "Logined in.";
+                    await _SignInManager.SignInAsync(user, loginVM.RememberMe);
+                    return RedirectToAction("Index", "Home", new { area = "Customer" });
+                }
+            }
+
+            TempData["error"] = "User name or email is wrong.";
+            ModelState.AddModelError(string.Empty, "User name or email is wrong.");
+            return View(loginVM);            
         }
 
         public async Task<IActionResult> ConfrimEmail(string userId, string token)
         {
 
             var user = await _UserManager.FindByIdAsync(userId);
-
 
             if (user is not null)
             {
@@ -80,6 +151,18 @@ namespace ETickets.Areas.Identity.Controllers
                 }
             }
 
+            return RedirectToAction(nameof(EmailConfrimed));
+        }
+
+        public IActionResult EmailConfrimed()
+        {
+            return View();
+        }
+
+        public  new async Task<IActionResult>  SignOut()
+        {
+            await _SignInManager.SignOutAsync();
+            TempData["seccuess"] = "Signed out.";
             return RedirectToAction("Index", "Home", new { area = "Customer" });
         }
 
---
+++ ModelView/LoginVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ETickets.ModelView
+{
+    public class LoginVM
+    {
+        [Required]
+        [Display(Name = "Username or email")]
+        public string UserNameOrEmail { get; set; } = null!;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+        public bool RememberMe{ get; set; }
+
+        public bool EmailConfirmed { get; set; } = true;
+
+
+    }
+}
