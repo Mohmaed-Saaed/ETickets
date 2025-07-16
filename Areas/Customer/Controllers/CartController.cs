5f3dd9359641b52a3310581d7962acf6b8facd5e
rowadalmadina2030 <rowadalmadina2030@gmail.com>
2025-07-16 16:48:31 +0300

    fix

--- Areas/Customer/Controllers/CartController.cs
+++ Areas/Customer/Controllers/CartController.cs
@@ -20,7 +20,7 @@ namespace ETickets.Areas.Customer.Controllers
         readonly public UserManager<ApplicationUser> _UserManager;
 
 
-        public CartController(UserManager<ApplicationUser>  userManager,
+        public CartController(UserManager<ApplicationUser> userManager,
                               ICartRepository cartRepository,
                               IMovieRepository movieRepository)
         {
@@ -34,9 +34,9 @@ namespace ETickets.Areas.Customer.Controllers
         {
             var user = await _UserManager.GetUserAsync(User);
 
-            if( user is not null)
+            if (user is not null)
             {
-                var userCart = _CartRepository.Get(c => c.ApplicationUserId == user.Id, [ x => x.Movie ]);
+                var userCart = _CartRepository.Get(c => c.ApplicationUserId == user.Id, [x => x.Movie]);
 
                 ViewBag.TotalAmount = userCart.Sum(x => x.Count * x.Movie.Price);
 
@@ -46,22 +46,24 @@ namespace ETickets.Areas.Customer.Controllers
 
             return new EmptyResult();
         }
-        public async Task<IActionResult> AddToCart(int movieId , int quantity)
+        public async Task<IActionResult> AddToCart(int movieId, int quantity)
         {
             var user = await _UserManager.GetUserAsync(User);
 
             if (user is not null)
             {
-                
+
                 var movie = _MovieRepository.GetOne(x => x.Id == movieId);
 
-                if (movie is not null) {
+                if (movie is not null)
+                {
 
-                    if(quantity == 0)
+                    if (quantity == 0)
                     {
                         TempData["error"] = "Add quantity.";
                         return RedirectToAction("Details", "Home", new { area = "Customer", Id = movieId });
-                    } else 
+                    }
+                    else
                     {
                         var cart = _CartRepository.GetOne(x => x.ApplicationUserId == user.Id && x.MovieId == movie.Id);
                         if (cart is not null)
@@ -78,36 +80,89 @@ namespace ETickets.Areas.Customer.Controllers
                                 Count = quantity
                             });
                         }
-                
+
                     }
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return RedirectToAction("Details", "Home", new { area = "Customer", Id = movieId });    
+            return RedirectToAction("Details", "Home", new { area = "Customer", Id = movieId });
         }
 
 
-        public async Task<JsonResult> Increment(int movieId)
+        public async Task<IActionResult> Increment(int movieId)
         {
 
-            var cartMovie = _CartRepository.GetOne(x => x.MovieId == movieId);
+            var user = await _UserManager.GetUserAsync(User);
 
-            if(cartMovie is not null)
+            if (user is not null)
             {
+                var cartMovie = _CartRepository.GetOne(x => x.MovieId == movieId && user.Id == x.ApplicationUserId);
 
-               var user = await _UserManager.GetUserAsync(User);
+                if (cartMovie is not null)
 
-                if(user is not null)
+
                 {
                     cartMovie.Count++;
                     _CartRepository.Commit();
 
-                    var TotalAmount =  _CartRepository.Get(x => x.ApplicationUserId == user.Id , [x => x.Movie]).Sum(x => x.Count * x.Movie.Price);
-
-                    return Json(new { status = true, movieCount = cartMovie.Count , TotalAmount });
+                    return RedirectToAction(nameof(Index));
                 }
             }
-            return Json(new {status = false});
+            return Json(new { status = false });
+        }
+
+        public async Task<IActionResult> Decrement(int movieId)
+        {
+
+            var user = await _UserManager.GetUserAsync(User);
+
+            if (user is not null)
+            {
+                var cartMovie = _CartRepository.GetOne(x => x.MovieId == movieId && user.Id == x.ApplicationUserId);
+
+                if (cartMovie is not null)
+                {
+                   
+
+                    cartMovie.Count--;
+
+                    if (cartMovie.Count == 0)
+                    {
+                        return RedirectToAction(nameof(Delete), "Cart", new { area = "Customer", movieId });
+                    }
+
+                    _CartRepository.Commit();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return Json(new { status = false });
+        }
+
+
+        public async Task<IActionResult> Delete(int movieId)
+        {
+
+            var cartMovie = _CartRepository.GetOne(x => x.MovieId == movieId);
+
+            if (cartMovie is not null)
+            {
+
+                var user = await _UserManager.GetUserAsync(User);
+
+                if (user is not null)
+                {
+
+                    var userCart = _CartRepository.GetOne(x => x.MovieId == movieId && user.Id == x.ApplicationUserId);
+
+                    if (userCart is not null)
+                    {
+                        _CartRepository.Delete(userCart);
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+            TempData["error"] = "Somthing is wrong";
+            return RedirectToAction(nameof(Index));
         }
 
     }
